921a8ef6dab3923a2481fa03a4def23196c4e682
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-08 10:22:02 -0700

    Linked database

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -18,7 +18,7 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-      List<Clinet> model = _db.Client.Include(Client => Client.Stylist).ToList();
+      List<Client> model = _db.Client.Include(Client => Client.Stylist).ToList();
       return View(model);
     }
 
@@ -31,21 +31,21 @@ namespace HairSalon.Controllers
     [HttpPost]
     public ActionResult Create(Client Client)
     {
-      _db.Clinet.Add(Client);
+      _db.Client.Add(Client);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-      Clinet
+      Client
        thisClient = _db.Client.FirstOrDefault(Client => Client.ClientId == id);
       return View(thisClient);
     }
 
     public ActionResult Edit(int id)
     {
-      var thisClient = _db.Clinet.FirstOrDefault(Client => Client.ClientId == id);
+      var thisClient = _db.Client.FirstOrDefault(Client => Client.ClientId == id);
       ViewBag.StylistId = new SelectList(_db.Stylist, "StylistId", "Name");
       return View(thisClient);
     }
@@ -60,15 +60,15 @@ namespace HairSalon.Controllers
 
     public ActionResult Delete(int id)
     {
-      var thisClient = _db.Clinet.FirstOrDefault(Client => Client.ClientId == id);
+      var thisClient = _db.Client.FirstOrDefault(Client => Client.ClientId == id);
       return View(thisClient);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisClient = _db.Clinet.FirstOrDefault(Client => Client.ClientId == id);
-      _db.Clinet.Remove(thisClient);
+      var thisClient = _db.Client.FirstOrDefault(Client => Client.ClientId == id);
+      _db.Client.Remove(thisClient);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
@@ -82,8 +82,8 @@ namespace HairSalon.Controllers
     public ActionResult Search(string description)
     {
       string searchDescription = description.ToLower();
-      List<Clinet
-      > searchResults = _db.Clinet.Where(Client => Client.Description.ToLower().Contains(searchDescription)).ToList();
+      List<Client
+      > searchResults = _db.Client.Where(Client => Client.Description.ToLower().Contains(searchDescription)).ToList();
       return View("Index", searchResults);
     }
   }
--- Models/Stylist.cs
+++ Models/Stylist.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-namespace HairSalon
+namespace HairSalon.Models
 {
   public class Stylist
   {
