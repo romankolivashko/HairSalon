f5d39618400dc6f7f398159076c695a5d578888a
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-08 09:21:14 -0700

    Added views

--- Models/HairSalonContext.cs
+++ Models/HairSalonContext.cs
@@ -5,7 +5,7 @@ namespace HairSalon.Models
   public class HairSalonContext : DbContext
   {
     public DbSet<Stylist> Stylist { get; set; }
-    public DbSet<Client> Clients { get; set; }
+    public DbSet<Client> Client { get; set; }
 
     public HairSalonContext(DbContextOptions options) : base(options) { }
 
